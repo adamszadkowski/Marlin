9484d9ed410f0268222211cb3e3b582906c8f947
Adam Szadkowski <adam@szadkowski.info>
2020-04-02 21:33:03 +0200

    Move bed by 25mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1109,10 +1109,10 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -33
+#define X_MIN_POS -8
 #define Y_MIN_POS -3
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS (X_BED_SIZE + 25)
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 240
 
